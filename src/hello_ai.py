ab7c1a43f529f2d788c9f3dc3f725ce2634c665a
Pablo Perez <perez51160900@hotmail.com>
2026-06-14 17:49:31 +0200

    feat: add token usage and cost calculation breakdown to completion response

--- src/hello_ai.py
+++ src/hello_ai.py
@@ -10,7 +10,8 @@ client = OpenAI()
 response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "user", "content": "Di hello world en tres idiomas (ch, en, fr)"}
+        {"role": "user",
+            "content": "Di hello world en tres idiomas (ch, en, fr)"}
     ]
 )
 
@@ -18,3 +19,19 @@ response = client.chat.completions.create(
 text = response.choices[0].message.content
 
 print(text)
+
+
+print("\n--Uso de tokens--\n")
+print(f"Tokens de Input: {response.usage.prompt_tokens}")
+print(f"Tokens de Output: {response.usage.completion_tokens}")
+print(f"Tokens totales: {response.usage.total_tokens}")
+
+
+cost_input = (response.usage.prompt_tokens / 1_000_000) * 0.15
+cost_output = (response.usage.completion_tokens / 1_000_000) * 0.60
+cost_total = cost_input + cost_output
+
+print(f"\n--Costos--\n")
+print(f"Costo Input: {cost_input} USD")
+print(f"Costo Output: {cost_output} USD")
+print(f"Costo Total: {cost_total} USD")
