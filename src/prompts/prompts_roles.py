126c04eb055f3dfc8ed2adc52fc83f65ff165793
Pablo Perez <perez51160900@hotmail.com>
2026-06-15 00:53:02 +0200

    feat: add demonstration of system role behavior with grumpy mathematician persona

--- src/prompts/prompts_roles.py
+++ src/prompts/prompts_roles.py
@@ -29,6 +29,30 @@ def show_roles():
 
     print(f"Respuesta del User: {response_1.choices[0].message.content}\n")
 
+    print("="*50)
+    print("Rol: System")
+    print("="*50)
+
+    response_2 = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": """"
+                Eres un matematico gruñon que contesta preguntas simples con desden 
+                pero con una precision absoluta , y a las preguntas muy muy sencillas les incluyes un comentario
+                digno del dr house 
+                """
+            },
+            {
+                "role": "user",
+                "content": "¿Cuanto es 2 + 2"
+            }
+        ]
+    )
+
+    print(f"Respuesta del System: {response_2.choices[0].message.content}\n")
+
 
 if __name__ == "__main__":
     show_roles()
